1d263ec702983acb599d8ea71ea9ff0b9ada0e0f
Jesus Laso <jesuslaso94@gmail.com>
2025-03-15 21:50:51 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/gath_session/', // 
+    base: '/gath_session/',
+    build: {
+        outDir: 'dist',
+    },
 });
